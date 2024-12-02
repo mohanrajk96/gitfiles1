8edcafcdb528676bab977e555da7dae3ebe50d7a
mohanrajk96 <mohanrajkofficial96@gmail.com>
2024-12-02 12:44:55 +0530

    first commit

---
+++ src/programs.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class programs {
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print ("Enter No: ");
+		int n = sc.nextInt();
+		//int n=30;
+		for (int i=1; i<=n; i++)
+		{
+			if(i%2==0)
+			{
+				System.out.print(" " +i);
+				
+			}
+		}
+	}
+	
+	
+}
